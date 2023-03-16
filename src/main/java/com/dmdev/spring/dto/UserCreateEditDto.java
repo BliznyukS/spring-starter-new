f04cd5014adf744dae7feeb5f78247377eb8fba7
Blyzniuk Serhii <bliznyuk.sergey@gmail.com>
2023-03-16 19:17:27 +0200

    Web Starter - end

--- src/main/java/com/dmdev/spring/dto/UserCreateEditDto.java
+++ src/main/java/com/dmdev/spring/dto/UserCreateEditDto.java
@@ -3,12 +3,16 @@ package com.dmdev.spring.dto;
 import com.dmdev.spring.database.entity.Company;
 import com.dmdev.spring.database.entity.Role;
 import lombok.Value;
+import lombok.experimental.FieldNameConstants;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Value
+@FieldNameConstants
 public class UserCreateEditDto {
     String username;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     LocalDate birthDate;
     String firstname;
     String lastname;
