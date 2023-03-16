9e8391c40aec290573a1e277283e0617980b6a22
Blyzniuk Serhii <bliznyuk.sergey@gmail.com>
2023-03-16 15:56:41 +0200

    Web Starter - CRUD SL

---
+++ src/main/java/com/dmdev/spring/dto/LoginDto.java
@@ -0,0 +1,8 @@
+package com.dmdev.spring.dto;
+
+import lombok.Value;
+@Value
+public class LoginDto {
+    String username;
+    String password;
+}
--- src/main/java/com/dmdev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dmdev/spring/http/controller/GreetingController.java
@@ -1,53 +1,75 @@
 package com.dmdev.spring.http.controller;
 
+import com.dmdev.spring.database.entity.Role;
 import com.dmdev.spring.database.repository.CompanyRepository;
 import com.dmdev.spring.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/v1")
 @SessionAttributes({"user"})
 public class GreetingController {
 
+    @ModelAttribute("roles")
+    public List<Role> roles(){
+        return Arrays.asList(Role.values());
+    }
+
 
     @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
+    public String hello(Model model, HttpServletRequest request,
+                        @ModelAttribute("userReadDto") UserReadDto userReadDto) {
 
-        modelAndView.setViewName("greeting/hello");
-
-        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
-        return modelAndView;
+        model.addAttribute("user", new UserReadDto(1L, "Ivan"));
+        return "greeting/hello";
     }
 
-
-    @GetMapping("/bye")
-    public ModelAndView bye(@SessionAttribute("user") UserReadDto user) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("greeting/bye");
-
-        return modelAndView;
-    }
-
-
-//    @GetMapping("/hello/{id}")
-//    public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request,
-//                              @RequestParam Integer age,
-//                              @RequestHeader String accept,
-//                              @CookieValue("JSESSIONID") String jsessionid,
-//                              @PathVariable("id") Integer id) {
-//        String ageParamValue = request.getParameter("age");
-//        String acceptParamValue = request.getHeader("accept");
-//        Cookie[] cookies = request.getCookies();
+//    @GetMapping("/hello")
+//    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
 //
 //        modelAndView.setViewName("greeting/hello");
 //
+//        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
 //        return modelAndView;
 //    }
+
+
+    @GetMapping("/bye")
+    public String bye(@SessionAttribute("user") UserReadDto user, Model model) {
+
+        return "greeting/bye";
+    }
+
+//    @GetMapping("/bye")
+//    public ModelAndView bye(@SessionAttribute("user") UserReadDto user) {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.setViewName("greeting/bye");
+//
+//        return modelAndView;
+//    }
+
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request,
+                              @RequestParam Integer age,
+                              @RequestHeader String accept,
+                              @CookieValue("JSESSIONID") String jsessionid,
+                              @PathVariable("id") Integer id) {
+        String ageParamValue = request.getParameter("age");
+        String acceptParamValue = request.getHeader("accept");
+        Cookie[] cookies = request.getCookies();
+
+        modelAndView.setViewName("greeting/hello");
+
+        return modelAndView;
+    }
 
 
 }
