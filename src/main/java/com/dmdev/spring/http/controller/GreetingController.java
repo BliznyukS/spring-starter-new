e40171e37442d0129ea55fa79b636b054ea20558
Blyzniuk Serhii <bliznyuk.sergey@gmail.com>
2023-03-18 14:29:03 +0200

    Thymeleaf Starter

--- src/main/java/com/dmdev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dmdev/spring/http/controller/GreetingController.java
@@ -1,7 +1,5 @@
 package com.dmdev.spring.http.controller;
-
 import com.dmdev.spring.database.entity.Role;
-import com.dmdev.spring.database.repository.CompanyRepository;
 import com.dmdev.spring.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,57 +17,41 @@ import java.util.List;
 public class GreetingController {
 
     @ModelAttribute("roles")
-    public List<Role> roles(){
+    public List<Role> roles() {
         return Arrays.asList(Role.values());
     }
 
-
     @GetMapping("/hello")
-    public String hello(Model model, HttpServletRequest request,
+    public String hello(Model model,
+                        HttpServletRequest request,
                         @ModelAttribute("userReadDto") UserReadDto userReadDto) {
+//        request.getSession().setAttribute(); sessionScope
+//        request.setAttribute(); requestScope
+//        request.getSession().getAttribute("user")
+        model.addAttribute("user", userReadDto);
 
-//        model.addAttribute("user", new UserReadDto(1L, "Ivan"));
         return "greeting/hello";
     }
 
-//    @GetMapping("/hello")
-//    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
-//
-//        modelAndView.setViewName("greeting/hello");
-//
-//        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
-//        return modelAndView;
-//    }
-
-
     @GetMapping("/bye")
     public String bye(@SessionAttribute("user") UserReadDto user, Model model) {
-
+//        request.getSession().getAttribute("user")
         return "greeting/bye";
     }
 
-//    @GetMapping("/bye")
-//    public ModelAndView bye(@SessionAttribute("user") UserReadDto user) {
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("greeting/bye");
-//
-//        return modelAndView;
-//    }
-
     @GetMapping("/hello/{id}")
     public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request,
-                              @RequestParam Integer age,
-                              @RequestHeader String accept,
-                              @CookieValue("JSESSIONID") String jsessionid,
-                              @PathVariable("id") Integer id) {
+                               @RequestParam Integer age,
+                               @RequestHeader String accept,
+                               @CookieValue("JSESSIONID") String JSESSIONID,
+                               @PathVariable("id") Integer id) {
         String ageParamValue = request.getParameter("age");
-        String acceptParamValue = request.getHeader("accept");
+        String acceptHeader = request.getHeader("accept");
         Cookie[] cookies = request.getCookies();
 
         modelAndView.setViewName("greeting/hello");
 
         return modelAndView;
     }
-
-
 }
+
--- src/main/java/com/dmdev/spring/http/controller/UserController.java
+++ src/main/java/com/dmdev/spring/http/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.dmdev.spring.http.controller;
 
+import com.dmdev.spring.database.entity.Role;
 import com.dmdev.spring.dto.UserCreateEditDto;
+import com.dmdev.spring.service.CompanyService;
 import com.dmdev.spring.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
@@ -15,11 +18,12 @@ import org.springframework.web.server.ResponseStatusException;
 public class UserController {
 
     private final UserService userService;
+    private final CompanyService companyService;
 
     @GetMapping
     public String findAll(Model model) {
-        model.addAttribute("users", userService.findAll());
 //        model.addAttribute("users", userService.findAll(filter));
+        model.addAttribute("users", userService.findAll());
         return "user/users";
     }
 
@@ -28,14 +32,30 @@ public class UserController {
         return userService.findById(id)
                 .map(user -> {
                     model.addAttribute("user", user);
+                    model.addAttribute("roles", Role.values());
+                    model.addAttribute("companies", companyService.findAll());
                     return "user/user";
                 })
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @GetMapping("/registration")
+    public String registration(Model model, @ModelAttribute("user") UserCreateEditDto user) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        model.addAttribute("companies", companyService.findAll());
+        return "user/registration";
+    }
+
     @PostMapping
 //    @ResponseStatus(HttpStatus.CREATED)
-    public String create(@ModelAttribute UserCreateEditDto user) {
+    public String create(@ModelAttribute UserCreateEditDto user, RedirectAttributes redirectAttributes) {
+//        if (true) {
+//            redirectAttributes.addAttribute("username", user.getUsername());
+//            redirectAttributes.addAttribute("firstname", user.getFirstname());
+//            redirectAttributes.addFlashAttribute("user", user);
+//            return "redirect:/users/registration";
+//        }
         return "redirect:/users/" + userService.create(user).getId();
     }
 
@@ -50,9 +70,12 @@ public class UserController {
     //    @DeleteMapping("/{id}")
     @PostMapping("/{id}/delete")
     public String delete(@PathVariable("id") Long id) {
-       if (!userService.delete(id)){
-           throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-       }
+        if (!userService.delete(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
         return "redirect:/users";
     }
+
+
 }
+
